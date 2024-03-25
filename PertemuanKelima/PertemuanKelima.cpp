7ae9e651bb9294f1114193f7711419fa8e6fd44d
ZahwaAnritia <anritia15@gmail.com>
2024-03-25 10:37:18 +0700

    command menu

--- PertemuanKelima/PertemuanKelima.cpp
+++ PertemuanKelima/PertemuanKelima.cpp
@@ -30,6 +30,23 @@ int pembagian(int bil_1, int bil_2) {
 int main()
 {
 	int pilihan;
+	
+		system("cls");
+		cout << "==========Menu=======" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "Masukkan menu yang ingin di piulih (1/2/3/4/5): ";
+		cin >> pilihan;
 
+		switch (pilihan) {
+		case 1:
+			input();
+			cout << "Hasil Penjumlahan = " << penjumlahan(a, b) << endl;
+		}
+
+	
 	
 }
